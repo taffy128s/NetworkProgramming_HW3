37392c078fa1f541fdb7cc34dd76406ac91f5ebd
Taffy Cheng <taffy128s@gmail.com>
2016-05-19 02:50:11 +0800

    Initial commit.

---
+++ HW3_103062122_Ser.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/stat.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+int main() {
+	
+	return 0;
+}
---
+++ makefile
@@ -0,0 +1,8 @@
+CC = g++ -Wall -std=c++11
+
+compile:
+	$(CC) -o HW3_103062122_Ser HW3_103062122_Ser.cpp
+	$(CC) -o HW3_103062122_Cli HW3_103062122_Cli.cpp
+
+clean:
+	rm HW3_103062122_Ser HW3_103062122_Cli
