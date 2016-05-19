e8aa58f9bb1494ecf7d6a2b3f867db49fedede46
Taffy Cheng <taffy128s@gmail.com>
2016-05-20 02:46:19 +0800

    Change environment.

--- HW3_103062122_Cli.cpp
+++ HW3_103062122_Cli.cpp
@@ -13,27 +13,65 @@
 #include <sstream>
 #define MAX 2048
 
+void showMenu() {
+	
+}
+
 int main(int argc, char **argv) {
-    if (argc != 3) {
-        puts("Usage: a.out <ServerIP> <ServerPort>");
-        exit(0);
-    }
-    
-    int sockfd;
-    struct sockaddr_in servaddr;
-    
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(atoi(argv[2]));
-    inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
-    
-    connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
-    
-    while (1) {
-        puts("You can input the following commands:");
-        break;
-    }
-    
-    return 0;
+	if (argc != 3) {
+		puts("Usage: a.out <ServerIP> <ServerPort>");
+		exit(0);
+	}
+	
+	char sendline[MAX], command[MAX], recv[MAX];
+	int sockfd;
+	struct sockaddr_in servaddr;
+	
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(atoi(argv[2]));
+	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+	
+	if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+		puts("Connect error");
+		exit(0);
+	}
+	
+	puts("**********Welcome**********");
+	puts("[R]egister [L]ogin");
+	bzero(sendline, sizeof(sendline));
+	fgets(sendline, MAX, stdin);
+	if (!strcmp("R\n", sendline)) {
+		puts("Please enter a new pair of username and password.");
+		bzero(command, sizeof(command));
+		fgets(command, MAX, stdin);
+		strcat(sendline, command);
+		write(sockfd, sendline, strlen(sendline));
+		bzero(recv, sizeof(recv));
+		read(sockfd, recv, MAX);
+		if (!strcmp("ok", recv)) {
+			puts("Registered successfully.");
+		} else {
+			puts("Username is used.");
+			exit(0);
+		}
+	} else if (!strcmp("L\n", sendline)) {
+		
+	} else {
+		puts("You didn't input a valid command");
+		exit(0);
+	}
+	while (1) {
+		puts("You can input the following commands:");
+		showMenu();
+		puts("Your input:");
+		bzero(sendline, sizeof(sendline));
+		bzero(command, sizeof(command));
+		bzero(recv, sizeof(recv));
+		if (fgets(sendline, MAX, stdin) == NULL) break;
+		
+	}
+	
+	return 0;
 }
--- HW3_103062122_Ser.cpp
+++ HW3_103062122_Ser.cpp
@@ -11,49 +11,83 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <map>
+#include <vector>
 #define MAX 2048
 
+using namespace std;
+
+map<string, string> userAndPassword;
+vector<string> onlineUserList;
+string fdToUsername[FD_SETSIZE];
+
+void registerAccount(int sockfd, char *username, char *password) {
+	char sendline[MAX] = {0};
+	string userName = username;
+	string passWord = password;
+	fdToUsername[sockfd] = userName;
+	if (userAndPassword[userName] == "") {
+		userAndPassword[userName] = passWord;
+		onlineUserList.push_back(userName);
+		fdToUsername[sockfd] = userName;
+		puts("A user just registered.");
+		sprintf(sendline, "ok");
+		write(sockfd, sendline, strlen(sendline));
+	} else {
+		sprintf(sendline, "no");
+		write(sockfd, sendline, strlen(sendline));
+	}
+}
+
 void *run(void *arg) {
-    char recv[MAX] = {0};
-    int connfd;
-    connfd = *((int *) arg);
-    pthread_detach(pthread_self());
-    while (read(connfd, recv, MAX) > 0) {
-        
-        bzero(recv, sizeof(recv));
-    }
-    puts("A thread terminated.");
-    close(connfd);
-    return NULL;
+	char recv[MAX] = {0};
+	int connfd;
+	connfd = *((int *) arg);
+	pthread_detach(pthread_self());
+	while (read(connfd, recv, MAX) > 0) {
+		char command[MAX] = {0};
+		sscanf(recv, "%s", command);
+		if (!strcmp("R", command)) {
+			char username[100] = {0}, password[100] = {0};
+			sscanf(recv, "%*s%s%s", username, password);
+			registerAccount(connfd, username, password);
+		} else if (!strcmp("L", command)) {
+			
+		}
+		bzero(recv, sizeof(recv));
+	}
+	puts("A thread terminated.");
+	close(connfd);
+	return NULL;
 }
 
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        puts("Usage: a.out <Port>");
-        exit(0);
-    }
-    
-    const int on = 1;
-    int listenfd, connfd;
-    socklen_t clilen;
-    pthread_t tid;
-    struct sockaddr_in servaddr, cliaddr;
-    
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(atoi(argv[1]));
-    
-    bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
-    listen(listenfd, 1024);
-    
-    while (1) {
-        clilen = sizeof(cliaddr);
-        connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &clilen);
-        printf("Connection from: %s, port: %d\n", inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port));
-        pthread_create(&tid, NULL, &run, (void *) &connfd);
-    }
-    return 0;
+	if (argc != 2) {
+		puts("Usage: a.out <Port>");
+		exit(0);
+	}
+	
+	const int on = 1;
+	int listenfd, connfd;
+	socklen_t clilen;
+	pthread_t tid;
+	struct sockaddr_in servaddr, cliaddr;
+	
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(atoi(argv[1]));
+	
+	bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+	listen(listenfd, 1024);
+	
+	while (1) {
+		clilen = sizeof(cliaddr);
+		connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &clilen);
+		printf("Connection from: %s, port: %d\n", inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port));
+		pthread_create(&tid, NULL, &run, (void *) &connfd);
+	}
+	return 0;
 }
