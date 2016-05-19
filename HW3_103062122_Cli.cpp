fdd5c95ad6ed0790bf9c090263b56b61471cbb66
Taffy Cheng <taffy128s@gmail.com>
2016-05-20 00:33:57 +0800

    Change from tab to spaces.

--- HW3_103062122_Cli.cpp
+++ HW3_103062122_Cli.cpp
@@ -14,26 +14,26 @@
 #define MAX 2048
 
 int main(int argc, char **argv) {
-	if (argc != 3) {
-		puts("Usage: a.out <ServerIP> <ServerPort>");
-		exit(0);
-	}
-	
-	int sockfd;
-	struct sockaddr_in servaddr;
-	
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(atoi(argv[2]));
-	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
-	
-	connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
-	
-	while (1) {
-		puts("You can input the following commands:");
-		break;
-	}
-	
-	return 0;
+    if (argc != 3) {
+        puts("Usage: a.out <ServerIP> <ServerPort>");
+        exit(0);
+    }
+    
+    int sockfd;
+    struct sockaddr_in servaddr;
+    
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(atoi(argv[2]));
+    inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+    
+    connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+    
+    while (1) {
+        puts("You can input the following commands:");
+        break;
+    }
+    
+    return 0;
 }
